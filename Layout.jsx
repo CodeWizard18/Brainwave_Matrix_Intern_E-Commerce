83ffceaabd07ecbcd178f742d4abe7d678370bf2
HARDIK BHARDWAJ <126346437+CodeWizard18@users.noreply.github.com>
2025-06-22 19:43:08 +0530

    Add files via upload

---
+++ Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { Outlet }  from "react-router-dom";
+import Footer from '../components/Footer';
+import { MdBoy, MdConstruction } from 'react-icons/md'
+import { LuConstruction } from 'react-icons/lu'
+function Layout() {
+    return ( 
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+            <div className='credit'>
+                Copyright Ⓒ 2025 Foodie. All Rights Reserved.
+                <div>
+                Crafted by Hardik Bhardwaj 2025
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Layout
